bafc38fe88b89d1b183a1becf354e00c17b8a132
Wonicon <conciselove@outlook.com>
2016-04-27 00:21:07 +0800

    Initial commit

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+# Clion
+set --export PATH $PATH $HOME/Downloads/clion-2016.1/bin
+# CMake 3.3
+set --export PATH $PATH $HOME/Downloads/clion-2016.1/bin/cmake/bin
+
+# MIPS cross compiler
+set --export PATH $PATH $HOME/mgc/embedded/codebench/bin
+
+# Init vivado
+set --export XILINX_VIVADO $HOME/Xilinx/Vivado/2015.2
+set --export PATH $PATH $HOME/Xilinx/Vivado/2015.2/bin
+
+# QEMU
+set --export PATH $PATH $HOME/qemu/bin/debug/native/i386-softmmu
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,33 @@
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _is_git_dirty
+  echo (command git status -s --ignore-submodules=all ^/dev/null)
+end
+
+function fish_prompt
+    set -l none (set_color normal)
+    set -l red (set_color -o red)
+    set -l yellow_fish (set_color -o FFB732)
+    
+    set_color $fish_color_cwd
+    
+    echo -sn $yellow_fish (basename (pwd))
+    
+    if [ (_git_branch_name) ]
+        echo -sn $red " " (_git_branch_name)
+        if [ (_is_git_dirty) ]
+            echo -sn (set_color normal) " (╯°Д°)╯ "
+        else
+            echo -sn (set_color normal) " (*´ ▽ `*) "
+        end
+    else
+        echo -sn (set_color normal) " (´ ∀ `) "
+    end
+
+    if [ $status != 0 ]
+        echo -sn $red "! " (set_color normal)
+    end
+
+end
